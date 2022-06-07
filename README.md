3a1d99f155533eaf61715839c16b230c82d0bee4
shehaan-avishka <shehaan.avishka00@gmail.com>
2022-06-07 11:17:24 +0530

    weather application 1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weather-Application
+Through this project, I had get knowledge of how to use API keys in web development.
+To get api key, used openweathermap.org website.
+That Api key provide information as the json file.
+Jason Viewer Chrome extensiton helped to read jason file.
+To get Background images used unsplash.com and created an account through that website able to get random image Url.
+Also Used Google fonts.
---
+++ script.js
@@ -0,0 +1,45 @@
+let weather = {
+  "apiKey" : "2201c6e0764b8290e53f804b94b37cff",
+  fetchWeather: function(city){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+
+            city + "&appid="+
+            this.apiKey+ "&units=metric&"
+          )
+          .then((Response ) => Response.json())
+          .then((data) => this.displayWeather(data));
+  },
+  displayWeather : function(data){
+    const {name} = data;
+    const { icon, description} = data.weather[0];
+    const {temp, humidity} = data.main;
+    const {speed} = data.wind;
+    console.log(name,icon,description,temp,humidity,speed);
+
+    //display in html
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".temp").innerText = temp +" °C";
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".wind").innerText = "Wind: " + speed + " Km/h";
+    document.querySelector(".humidity").innerText = "Humidity: "+ humidity +"%";
+    document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("London,UK");
